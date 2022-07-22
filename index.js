31f0b7b5fdd454b243c4b700381077766551d53d
sselvag <swaathy.selva@gmail.com>
2022-07-21 22:52:18 -0400

    completed index.js

--- index.js
+++ index.js
@@ -1,116 +1,153 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
-const generateHTML = require('./utils/generateHTML')
+const generateHTML = require('./utils/generateHTML');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
 
-const manager = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of the team manager?'
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is the team managers ID?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is the team managers email?'
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'What is the team managers office number?'
-    },
-    {
-        type: 'list',
-        name:'teamMember',
-        message: 'Would you like to add another employee?',
-        choices: ['Engineer', 'Intern', 'No, I am done']
-    }
-]
+const team = [];
 
-const engineer = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of the engineer?'
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is the engineers ID?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is the engineers email?'
-    },
-    {
-        type: 'input',
-        name: 'gitHub',
-        message: 'What is the engineers github?'
-    },
-    {
-        type: 'list',
-        name:'teamMember',
-        message: 'Would you like to add another employee?',
-        choices: ['Engineer', 'Intern', 'No, I am done']
-    }
-]
+const promptManager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the team manager?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the team managers ID?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the team managers email?'
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is the team managers office number?'
+        },
+        {
+            type: 'list',
+            name:'teamMember',
+            message: 'Would you like to add another employee?',
+            choices: ['Engineer', 'Intern', 'No, I am done']
+        }
+    ]).then (answers => {
+        const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+        team.push(manager);
+        if (answers.teamMember === 'Engineer') {
+            promptEngineer();
+        } else if (answers.teamMember === 'Intern') {
+            promptIntern();
+        } else {
+            writeToFile();
+        }
+    })
+    .catch(err => {
+        if (err) {
+            console.log(err)
+        }
+    })
+};
 
-const intern = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of the intern?'
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is the interns ID?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is the interns email?'
-    },
-    {
-        type: 'input',
-        name: 'school',
-        message: 'Which school does the intern attend?'
-    },
-    {
-        type: 'list',
-        name:'teamMember',
-        message: 'Would you like to add another employee?',
-        choices: ['Engineer', 'Intern', 'No, I am done']
-    }
-]
+const promptEngineer = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the engineer?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the engineers ID?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the engineers email?'
+        },
+        {
+            type: 'input',
+            name: 'gitHub',
+            message: 'What is the engineers github?'
+        },
+        {
+            type: 'list',
+            name:'teamMember',
+            message: 'Would you like to add another employee?',
+            choices: ['Engineer', 'Intern', 'No, I am done']
+        }
+    ]).then (answers => {
+        const engineer = new Engineer(data.name, data.id, data.email, data.gitHub);
+        team.push(engineer);
+        if (answers.teamMember === 'Engineer') {
+            promptEngineer();
+        } else if (answers.teamMember === 'Intern') {
+            promptIntern();
+        } else {
+            writeToFile();
+        }
+    })
+    .catch(err => {
+        if (err) {
+            console.log(err)
+        }
+    })
+};
 
-// function to loop through tm question
+const promptIntern = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the intern?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the interns ID?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the interns email?'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Which school does the intern attend?'
+        },
+        {
+            type: 'list',
+            name:'teamMember',
+            message: 'Would you like to add another employee?',
+            choices: ['Engineer', 'Intern', 'No, I am done']
+        }
+    ]).then (answers => {
+        const intern = new Intern(data.name, data.id, data.email, data.school);
+        team.push(intern);
+        if (answers.teamMember === 'Engineer') {
+            promptEngineer();
+        } else if (answers.teamMember === 'Intern') {
+            promptIntern();
+        } else {
+            writeToFile();
+        }
+    })
+    .catch(err => {
+        if (err) {
+            console.log(err)
+        }
+    })
+};
 
 function writeToFile() {
-    return fs.writeFile('./dist/', generateHTML);
+    return fs.writeFile('./dist/index.html', generateHTML(team));
 }
 
-function repeatQuestions(data) {
-    if (data.teamMember === 'Engineer') {
-        inquirer.prompt(engineer)
-    } else if (data.teamMember === 'Intern') {
-        inquirer.prompt(intern)
-    } else if (data.teamMember === 'No, I am done') {
-        writeToFile();
-    };
-}
-
-// initiate function
-
-function init() {
-    inquirer.prompt(manager);
-}
-
-// function for no i am done
-init();
+promptManager();
 
